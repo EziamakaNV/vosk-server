64dcc1f4cfec919aff873a845d5b1c1ec9888900
Nnaemeka Eziamaka <eziamakanv@gmail.com>
2021-06-09 22:38:20 +0100

    push

---
+++ client-samples/node/server.js
@@ -0,0 +1,42 @@
+
+const express = require('express');
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+
+
+// const whitelist = ['https://harvesters.yambarn.io']
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     if (whitelist.indexOf(origin) !== -1 || !origin) {
+//       callback(null, true)
+//     } else {
+//       callback(new Error('Not allowed by CORS'))
+//     }
+//   },
+//   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+// }
+
+// app.use(cors());
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(process.cwd(), 'public', 'index.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(process.cwd(), 'public', 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}`);
+})
+
+module.exports = app;
